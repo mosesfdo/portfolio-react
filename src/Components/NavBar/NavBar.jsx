49d5c47f5b9a719cdcc383280b5f55255a247d80
Moses <dmosesfernando@gmail.com>
2025-07-03 16:58:46 +0530

    done half

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -12,8 +12,9 @@ function NavBar() {
         <div className="Navlinks">
             <ul>
                 <li><a href="#home">Home</a></li>
-                <li><a href="#about">About</a></li>
                 <li><a href="#projects">Projects</a></li>
+                <li><a href="#about">About</a></li>
+                
                 <li><a href="#contact">Contact</a></li>
             </ul>
         </div>
