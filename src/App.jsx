b55f0b317c4b87a2d2074f0717456679aaae59a4
Moses <dmosesfernando@gmail.com>
2025-07-03 15:26:42 +0530

    done home

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,58 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
+import NavBar from './Components/NavBar/NavBar'
+import Waves from './Components/Waves/Waves';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
+    <div className="app-root">
+      <Waves
+        lineColor="rgba(255, 255, 255, 0.21)"
+        backgroundColor="rgb(0, 0, 0)"
+        waveSpeedX={0.02}
+        waveSpeedY={0.01}
+        waveAmpX={40}
+        waveAmpY={20}
+        friction={0.9}
+        tension={0.01}
+        maxCursorMove={120}
+        xGap={12}
+        yGap={36}
+        opacity={0.2}
+      />
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <NavBar />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      <section id="home" className="hero wrapper">
+        <div className="main-hero-text">
+          <h2>"I build apps that do the boring stuff
+            —so I can pretend I'm being productive."</h2>
+          <p>— yokoso —</p>
+        </div>
+      </section>
+
+      <section id="about" className="about wrapper">
+            <img src="src\assets\about-image.jpg" alt="about-image" className="about-img" />
+            <h2>"And he has filled him with the Spirit of God, with wisdom, with understanding,
+                 with knowledge and with all kinds of skills—to make artistic 
+                 designs for work in gold, silver and bronze..."</h2>
+            <p className="verse-mark">— Exodus 35:31-32</p>
+            <p>I've always been curious by nature. While most people around me 
+                just used computers, I found myself digging deeper—trying to
+                 understand how things worked under the hood. I was constantly 
+                 exploring, experimenting, and learning more than my peers. 
+                 One thing that always frustrated me? Finding the right software,
+                  app, or tool for what I needed. Nothing ever seemed to do exactly
+                   what I wanted. That's when it hit me—'Why not build my own?' 
+                   A simple idea, maybe even a little wild at the time—but it
+                    changed everything. It pushed me to learn how to design and
+                     develop products myself. That spark turned into a passion. 
+                     Now, I don't just look for solutions
+
+                    </p>
+                    <p>—I create them.</p>
+      </section>
+    </div>
   )
 }
 
---
+++ src/Components/NavBar/NavBar.jsx
@@ -0,0 +1,31 @@
+import React from 'react'   
+import './NavBar.css'
+
+function NavBar() {
+  return (
+    <div className="Navbar">
+        <div className="TopLogo">
+            <a href="#">mosesfdo</a>
+            <p>Full Stack Developer</p>
+        </div>
+
+        <div className="Navlinks">
+            <ul>
+                <li><a href="#home">Home</a></li>
+                <li><a href="#about">About</a></li>
+                <li><a href="#projects">Projects</a></li>
+                <li><a href="#contact">Contact</a></li>
+            </ul>
+        </div>
+
+    </div>
+
+
+
+
+
+
+  )
+}
+
+export default NavBar
