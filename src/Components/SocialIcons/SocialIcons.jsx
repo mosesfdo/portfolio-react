6c0f620d9fd3ab3aeaff95be6956603759d56284
Moses <dmosesfernando@gmail.com>
2025-07-12 21:28:53 +0530

    Fix resume link in SocialIcons component
    Updated the resume import to reference the PDF file and changed the download link to use the imported resume variable, ensuring the resume downloads correctly.

--- src/Components/SocialIcons/SocialIcons.jsx
+++ src/Components/SocialIcons/SocialIcons.jsx
@@ -2,14 +2,14 @@ import React from 'react';
 import './SocialIcons.css';
 import linkedin from '../../assets/linkedin.svg';
 import github from '../../assets/github.svg';
-import resume from '../../assets/resume.svg';
+import resume from '../../assets/resume.pdf';
 
 function SocialIcons() {
   return (
     <div className="social-icons">
         <a href="https://www.linkedin.com/in/mosesfdo/" target="_blank" rel="noopener noreferrer"><img src={linkedin} alt="linkedin" /></a>
         <a href="https://github.com/mosesfdo" target="_blank" rel="noopener noreferrer"><img src={github} alt="github" /></a>
-        <a href="/src/assets/resume.pdf" download>
+        <a href={resume} download>
             <img src={resume} alt="resume" />
         </a>
     </div>
