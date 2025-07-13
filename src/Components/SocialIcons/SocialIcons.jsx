db52a564c8cf40e693bdabc4920fd4e156b682b1
Moses <dmosesfernando@gmail.com>
2025-07-13 22:12:04 +0530

    Use SVG icon for resume download link
    Replaces the resume PDF thumbnail with a dedicated SVG icon for the resume download link in SocialIcons component for improved visual consistency.

--- src/Components/SocialIcons/SocialIcons.jsx
+++ src/Components/SocialIcons/SocialIcons.jsx
@@ -3,6 +3,7 @@ import './SocialIcons.css';
 import linkedin from '../../assets/linkedin.svg';
 import github from '../../assets/github.svg';
 import resume from '../../assets/resume.pdf';
+import resumeIcon from '../../assets/resume.svg';
 
 function SocialIcons() {
   return (
@@ -10,7 +11,7 @@ function SocialIcons() {
         <a href="https://www.linkedin.com/in/mosesfdo/" target="_blank" rel="noopener noreferrer"><img src={linkedin} alt="linkedin" /></a>
         <a href="https://github.com/mosesfdo" target="_blank" rel="noopener noreferrer"><img src={github} alt="github" /></a>
         <a href={resume} download>
-            <img src={resume} alt="resume" />
+            <img src={resumeIcon} alt="resume" />
         </a>
     </div>
   )
