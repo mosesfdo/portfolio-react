fa99f0987808cbedb5f44d5eef996d039c619bad
Moses <dmosesfernando@gmail.com>
2025-08-08 15:38:18 +0530

    Update NavBar.jsx

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -34,7 +34,6 @@ function NavBar({ scrollToSection }) {
                 <li><a href="#home" className="nav-anchor" onClick={scrollToSection}>Home</a></li>
                 <li><a href="#projects" className="nav-anchor" onClick={scrollToSection}>Projects</a></li>
                 <li><a href="#about" className="nav-anchor" onClick={scrollToSection}>About</a></li>
-                <li><a href="#contact" className="nav-anchor" onClick={scrollToSection}>Contact</a></li>
             </ul>
         </div>
 
